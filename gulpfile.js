56acdf5bbeec1244cc3b9ea51b56ae6cab017db0
Mark Reveley <markreveley@v-w-e.local>
2018-07-28 13:11:50 -0700

    gulp and nunjucks added

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var nunjucksRender = require('gulp-nunjucks-render');
+
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['app/templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('app'))
+});
+
+gulp.task('default', [ 'nunjucks' ]);
